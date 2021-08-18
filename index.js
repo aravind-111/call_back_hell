0d92ccfc3f9153395ab8eeda62403e6005107ee2
Aravindan-111 <aravindhan11195@gmail.com>
2021-08-18 08:03:21 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+setTimeout(() => {
+  document.getElementById("count").innerHTML = "10";
+  setTimeout(() => {
+    document.getElementById("count").innerHTML = "9";
+    setTimeout(() => {
+      document.getElementById("count").innerHTML = "8";
+      setTimeout(() => {
+        document.getElementById("count").innerHTML = "7";
+        setTimeout(() => {
+          document.getElementById("count").innerHTML = "6";
+          setTimeout(() => {
+            document.getElementById("count").innerHTML = "5";
+            setTimeout(() => {
+              document.getElementById("count").innerHTML = "4";
+              setTimeout(() => {
+                document.getElementById("count").innerHTML = "3";
+                setTimeout(() => {
+                  document.getElementsByClassName("count").innerHTML = "2";
+                  setTimeout(() => {
+                    document.getElementById("count").innerHTML = "1";
+                    setTimeout(() => {
+                      document.getElementById("count").innerHTML = "HAPPY INDEPENDANCE DAY";
+                    }, 1000)
+                  }, 1000)
+                }, 1000)
+              }, 1000)
+            }, 1000)
+          }, 1000)
+        }, 1000)
+      }, 1000)
+    }, 1000)
+  }, 1000)
+}, 1000)
